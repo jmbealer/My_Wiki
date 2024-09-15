5ec871a8307cb859186bdfa6473fba02418d855d
Justin Bealer <jmbealer11@gmail.com>
2024-09-15 14:29:57 -0500

    some cleanup

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -10,11 +10,14 @@ export default (() => {
     return (
       <div id="links-header-container">
         <div id="links-header">
-          <a class="links-header-item" href="/guides/">Guides</a>
-          <a class="links-header-item" href="/modding-tools/">Tools</a>
-          <a class="links-header-item" href="https://tes3cs.pages.dev/">Construction Set</a>
-          <a class="links-header-item" href="/programmers-reference/">Programmers Reference</a>
-          <a class="links-header-item" href="/contributing/"> Contributing</a>
+          {/* <a class="links-header-item" href="/guides/">Guides</a> */}
+          <a class="links-header-item" href="/project/">Projects</a>
+          {/* <a class="links-header-item" href="/modding-tools/">Tools</a> */}
+          <a class="links-header-item" href="/resume/">Resume</a>
+          <a class="links-header-item" href="/about/">About</a>
+          {/* <a class="links-header-item" href="https://tes3cs.pages.dev/">Construction Set</a> */}
+          {/* <a class="links-header-item" href="/programmers-reference/">Programmers Reference</a> */}
+          {/* <a class="links-header-item" href="/contributing/"> Contributing</a> */}
         </div>
         <hr></hr>
       </div>
