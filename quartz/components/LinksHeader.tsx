56338a0033fa0ecba4afb5b2f26bdcf68afccaf3
Justin Bealer <jmbealer11@gmail.com>
2024-09-16 11:16:21 -0500

    working on linkheader

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -10,32 +10,29 @@ export default (() => {
     return (
       <div>
         <div id="links-header">
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Card%20index/Color/card_index_color.svg"></img>
-            <a href="https://camargomau.com/">Blog</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img>
-            <a href="/Sciujo/MAC/MAC">MAC</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img>
-            <a href="/Sciujo/Sciujo">Learn</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Parrot/Color/parrot_color.svg"></img>
-            <a href="/Lingvujo/Lingvujo">Langs</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Triangular%20ruler/Color/triangular_ruler_color.svg"></img>
-            <a href="/Sciujo/Mathematics">Maths</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Laptop/Color/laptop_color.svg"></img>
-            <a href="/Sciujo/Computing">Comp</a>
-          </span>
+          {/* <span> */}
+          {/*   <a href="https://camargomau.com/">Blog</a> */}
+          {/* </span> */}
+          <span><a href="/projects">Projects</a></span>
+          <span><a href="/resume/">Resume</a></span>
+          <span> <a href="/about">About</a></span>
+          {/* <span> */}
+          {/*   <a href="/Lingvujo/Lingvujo">Langs</a> */}
+          {/* </span> */}
+          {/* <span> */}
+          {/*   <a href="/Sciujo/Mathematics">Maths</a> */}
+          {/* </span> */}
+          {/* <span> */}
+          {/*   <a href="/Sciujo/Computing">Comp</a> */}
+          {/* </span> */}
+          {/* <span> */}
+          {/*   <a href="/Sciujo/Computing">Comp</a> */}
+          {/* </span> */}
+          {/* <span> */}
+          {/*   <a href="/Sciujo/Computing">Comp</a> */}
+          {/* </span> */}
         </div>
-      <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
+        <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
       </div>
     )
   }
