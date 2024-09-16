6265774b945eaa204f4d4d820d83e0438120c6f7
Justin Bealer <jmbealer11@gmail.com>
2024-09-16 12:37:38 -0500

    updating linksheader and content

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -13,9 +13,7 @@ export default (() => {
           {/* <span> */}
           {/*   <a href="https://camargomau.com/">Blog</a> */}
           {/* </span> */}
-          <span><a href="/projects/pro.md">Projects</a></span>
-          <span><a href="/projects/pro">foo</a></span>
-          <span><a href="/projects/pro/">bar</a></span>
+          <span><a href="/projects">Projects</a></span>
           <span><a href="/resume/">Resume</a></span>
           <span> <a href="/about/">About</a></span>
           {/* <span> */}
